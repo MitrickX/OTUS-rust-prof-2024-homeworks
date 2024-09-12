f36888ca20c186a721a9d5c0711e08324d41a0b0
Dmitry Pavlov <mitrickx@mail.ru>
2024-09-12 08:16:38 +0300

    hw2: smart devices prototypes

---
+++ hw2/src/lib.rs
@@ -0,0 +1,33 @@
+struct _SmartPlug {
+    description: String,
+    is_on: bool,
+    current_power: f64,
+}
+
+impl _SmartPlug {
+    fn _description(&self) -> &str {
+        todo!()
+    }
+
+    fn _turn_on(&mut self) {
+        todo!()
+    }
+
+    fn _turn_off(&mut self) {
+        todo!()
+    }
+
+    fn _current_power(&self) -> f64 {
+        todo!()
+    }
+}
+
+struct _Thermometer {
+    current_temperature: f64,
+}
+
+impl _Thermometer {
+    fn _current_temperature(&self) -> f64 {
+        todo!()
+    }
+}
